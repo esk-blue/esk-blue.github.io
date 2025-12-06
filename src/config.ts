d3f1ab542e9d371a7ef71728565402d3a3897bb2
esk-blue <model7589@gmail.com>
2025-12-07 02:49:24 +0900

    Change siteConfig name and title
    Updated siteConfig with new name and title.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
+  name: "EUNSEONG KIM",
+  title: "AI Engineer",
   description: "Portfolio website of Ryan Fitzgerald",
   accentColor: "#1d4ed8",
   social: {
