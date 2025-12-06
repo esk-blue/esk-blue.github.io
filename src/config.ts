70041a25c9bac1e43b11c66f348b776fe8630467
esk-blue <model7589@gmail.com>
2025-12-07 03:12:12 +0900

    Update siteConfig with new personal information

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,12 @@
 export const siteConfig = {
   name: "EUNSEONG KIM",
   title: "AI Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  description: "Portfolio website of EUNSEONG KIM",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "model7589@gmail.com",
+    instagram: "https://www.instagram/eunseong__kim",
+    github: "https://github.com/esk-blue",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
